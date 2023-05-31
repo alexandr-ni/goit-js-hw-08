4f3b95679a9d0b1b91799bdb44447b452284dd53
alexandr-ni <sashanikitenko119@gmail.com>
2023-05-31 09:59:35 +0300

    complete first homework

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainerEl = document.querySelector('.gallery');
+const galleryItemEl = createGalleryItem(galleryItems);
 
-console.log(galleryItems);
+function createGalleryItem(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+       </a>
+    </li>`;
+    })
+    .join('');
+}
+
+galleryContainerEl.insertAdjacentHTML('beforeend', galleryItemEl);
+
+galleryContainerEl.addEventListener('click', onItemClick);
+
+function onItemClick(event) {
+  event.preventDefault();
+
+  if (event.target.classList.value !== 'gallery__image') {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox('.gallery__link', {
+    captionsData: 'alt',
+    captionDelay: '250',
+  });
+
+  galleryContainerEl.removeEventListener('click', onItemClick);
+}
