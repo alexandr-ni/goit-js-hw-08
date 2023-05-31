ea2dbbc436617409b457f91e1cfec5a74f8f94ba
alexandr-ni <sashanikitenko119@gmail.com>
2023-05-31 15:33:40 +0300

    complete homework two

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+function onPlay() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem(CURRENT_TIME_KEY, seconds);
+  });
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY));
